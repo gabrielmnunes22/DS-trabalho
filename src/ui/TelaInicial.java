01d4eff6d890d08f7fb2ca8122f0a24d791167c0
Gustavo-Camilotti <gustavocamilotti1@gmail.com>
2026-03-26 12:52:44 -0300

    TelaInicial adicionada em 'ui'.

---
+++ src/ui/TelaInicial.java
@@ -0,0 +1,15 @@
+package ui;
+
+
+
+/*
+  Esta classe será responsável por desenhar a tela do sistema.
+  Seu papel principal será capturar o texto de entrada digitado no campo de texto
+  e repassá-lo para o Parser processar, além de permitir que o usuário configure
+  os parâmetros iniciais da música (como BPM, instrumento e volume),
+  NADA ALEM DISSO
+   */
+
+public class TelaInicial
+{
+}
