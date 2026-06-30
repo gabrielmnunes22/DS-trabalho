87d2e8edb14ca2690e881315f1f0d1dedaa64166
JPKlippel <jp17112002@gmail.com>
2026-06-29 21:56:00 -0300

    - metodo legado

--- src/domain/services/Mapper.java
+++ src/domain/services/Mapper.java
@@ -35,49 +35,7 @@ public class Mapper {
         return new domain.actions.RepetirNotaAcao(); // segurança — nunca deve chegar aqui
     }
 
-    public AcaoMusical mapearCaractere(char caractere){
-
-        switch(caractere){
-            case 'A':
-            case 'B':
-            case 'C':
-            case 'D':
-            case 'E':
-            case 'F':
-            case 'G':
-                return new TocarNotaAcao(String.valueOf(caractere));
-
-            case 'H':
-                return new TocarNotaAcao("Bb");
-
-            case '!':
-                return new TrocarInstrumentoAcao(ACORDEAO);
-
-            case ';':
-                return new TrocarInstrumentoAcao(TUBULAR_BELLS);
-
-            case ',':
-                return new TrocarInstrumentoAcao(CHURCH_ORGAN);
-
-            case ' ':
-                return new DobrarVolumeAcao();
-
-            case '>':
-                return new MudarBpmAcao(ACELERACAO);
-            case '<':
-                return new MudarBpmAcao(DESACELARACAO);
-
-            case '?':
-                return new MudarOitavaAcao(1);
-            case 'V':
-                return new MudarOitavaAcao(-1);
-
-            default:
-                return null;
-            }
-        }
-
-        private final List<RegraDeCaractere> cadeia = Arrays.asList(
+    private final List<RegraDeCaractere> cadeia = Arrays.asList(
             new RegraMiBemol(),
             new RegraNotas(),
             new RegraNotaH(),
